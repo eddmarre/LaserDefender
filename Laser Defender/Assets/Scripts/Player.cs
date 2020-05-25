1b4f7f16bf0dcd91a546c958272d5560c92826d0
Eddie Marrero Jr <50213623+eddmarre@users.noreply.github.com>
2020-05-25 11:57:11 -0400

    Basic Movement

---
+++ Laser Defender/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var newXPos = transform.position.x + deltaX;
+
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        var nexYPos = transform.position.y + deltaY;
+        transform.position = new Vector2(newXPos, nexYPos);
+    }
+}
