c9053d5ebc3c3d686e04dbb6ef4bd2bc235c80bb
Eddie Marrero Jr <50213623+eddmarre@users.noreply.github.com>
2020-05-25 20:52:03 -0400

    Bullets do damage

---
+++ Laser Defender/Assets/Scripts/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float health = 100f;
+
+    private void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        health -= damageDealer.GetDamage();
+    }
+}
