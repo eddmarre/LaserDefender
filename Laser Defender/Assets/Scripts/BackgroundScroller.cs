fe8038bb9a549a14d0a2cd4a843233ff70905a3a
Eddie Marrero Jr <50213623+eddmarre@users.noreply.github.com>
2020-05-27 11:08:44 -0400

    Moving Background

---
+++ Laser Defender/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [SerializeField] float backgrondScrollSpeed = 5f;
+    Material myMaterial;
+    Vector2 offSet;
+
+    private void Start()
+    {
+        myMaterial = GetComponent<Renderer>().material;
+        offSet = new Vector2(0f, backgrondScrollSpeed);
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        myMaterial.mainTextureOffset += offSet * Time.deltaTime;
+    }
+}
